77e7273f109939295ee6f2fbed35211cb83a982c
Dagner Alena Guerra <dalenag87@gmail.com>
2018-09-08 05:36:54 -0400

    ok

--- application/views/emprendedor/capacitacion_videos.php
+++ application/views/emprendedor/capacitacion_videos.php
@@ -1,9 +1,9 @@
 <div class="row bg-title">
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Starter Page</h4> </div>
+                        <h4 class="page-title">CAPACITACION</h4> </div>
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                         <button class="right-side-toggle waves-effect waves-light btn-info btn-circle pull-right m-l-20"><i class="ti-settings text-white"></i></button>
-                        <a href="javascript: void(0);" target="_blank" class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Buy Admin Now</a>
+                        <a href="javascript: void(0);" target="_blank" class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Videos </a>
                         <ol class="breadcrumb">
                             <li><a href="#">Dashboard</a></li>
                             <li class="active">Starter Page</li>
@@ -18,21 +18,14 @@
     <div class="col-sm-12">
         <div class="white-box">
             <div class="row">
-                <div class="col-sm-4">
-                    <h3 class="box-title m-b-0">Modulo</h3>
-                    <div class="zoom-gallery m-t-30">
-                        <a href="../plugins/images/big/img5.jpg" title="Caption. Can be aligned to any side and contain any HTML."> <img src="<?php echo base_url();?>assets/plugins/images/big/img5.jpg" width="32.5%" /> </a>
-                        <a href="../plugins/images/big/img6.jpg" title="This image fits only horizontally."> <img src="<?php echo base_url();?>assets/plugins/images/big/img6.jpg" width="32.5%" /> </a>
-                        <a href=""> <img src="<?php echo base_url();?>assets/plugins/images/big/img4.jpg" width="32.5%" /> </a>
-                    </div>
-                </div>
-                <div class="col-sm-2"></div>
+                
                 <div class="col-sm-3">
                     <h3 class="box-title m-b-0">Video # 1</h3> 
                     <small>Luego de ver el video por favor rellenar el formulario de evaluación</code></small>
                     <br/>
                     <br/> <a class="popup-youtube btn btn-danger" href="www.youtube.com/watch?v=sK7riqg2mr4">Open YouTube video</a>
                     <br> </div>
+                    <div class="col-sm-2"></div>
            </div>
         </div>
     </div>
@@ -44,7 +37,7 @@
     <!-- .col -->
     <div class="col-sm-12">
         <div class="white-box">
-            <h3 class="box-title m-b-0">Form Popup</h3> <a class="popup-with-form btn btn-success" href="#test-form">Open form</a>
+            <h3 class="box-title m-b-0">Formulario de Evaluacion</h3> <a class="popup-with-form btn btn-success" href="#test-form">Open form</a>
             <!-- form itself -->
             <form id="test-form" class="mfp-hide white-popup-block">
                 <h1>Form</h1>
