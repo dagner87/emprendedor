8305489ddf1e4cf37fce59fe9351161f6310174e
Dagner Alena Guerra <dalenag87@gmail.com>
2018-09-12 17:41:35 -0400

    registro

--- application/controllers/Panel_admin.php
+++ application/controllers/Panel_admin.php
@@ -75,6 +75,16 @@ class Panel_admin extends CI_Controller
         $param['fecha_insc']   = date('Y-m-d');
         $data['id_hijo']      = $this->modelogeneral->insert_emp($param);
         $data['id_padre']     = $this->session->userdata('id_emp');
+
+        $nombre       = $this->session->userdata('nombre');
+        
+        $asunto = $nombre." te invita";
+        $cuerpo_mensaje = "Hola ".$param['nombre_emp']." te invito a que formes parte de nuestro negocio como emprendedor";
+        $url = "http://18.228.90.211/emprendedores/sistema/registro";
+        $cuerpo_mensaje .= "<a href='".$url."' target='_blank'> Completar Registro</a>";
+
+        $this->sendMailMandril($param['email'],$asunto, $cuerpo_mensaje);
+
         $result = $this->modelogeneral->insert_emp_asoc($data);
         $msg['comprobador'] = false;
         if($result)
@@ -103,6 +113,58 @@ class Panel_admin extends CI_Controller
         }
     
         echo $output;
+    }
+
+    public function cuerpo_mail($datos)
+    {
+        $message = '<html><head>';
+        $message .='<head>';
+        $message .='<meta charset="UTF-8">';
+        $message .='<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+        $message .='</head>';
+        $message = '<html><body>';
+        $message .= '<table  style=""  cellpadding="10">';
+        $message .= "<tr ><td bgcolor='#1696e0' colspan='2'><div align='center'><h3>Datos de Registro  </h3></div> </td></tr>";
+        $message .= "<tr style='background: #eee;'><td><strong>Fecha de registro:</strong> </td><td>" . $datos['fecha']. "</td></tr>";
+        $message .= "<tr style='background: #eee;'><td><strong>Usuario:</strong> </td><td>" .$datos['usuario']. "</td></tr>";
+        $message .= "<tr style='background: #eee;'><td><strong>Correo:</strong> </td><td>" .$datos['mail']. "</td></tr>";
+        $message .= "<tr style='background: #eee;'><td><strong>DNI:</strong> </td><td>" . $datos['dni']. "</td></tr>";
+        $message .= "<tr style='background: #eee;'><td><strong>Contrase&ntilde;a:</strong> </td><td>" . $datos['pass']. "</td></tr>";
+        $message .= "<tr ><td colspan='2'> <a href='http://panel.shoppingads.com.ar/' target='_blank'>Acceso al Panel</a></td></tr>";
+        $message .= "</table>";
+    
+        return  $message;
+    } 
+
+
+    public function sendMailMandril($email_destino,$asunto, $cuerpo_mensaje)
+    {       
+        //cargamos la libreria email de ci
+        $this->load->library("email");
+ 
+        //configuracion para gmail
+        $configGmail = array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'smtp.mandrillapp.com',
+            'smtp_port' => 587,
+            'smtp_user' => 'administracion@dvigi.com.ar',
+            'smtp_pass' => 'nt6mBSRsBN-LM9m0y5Lkcw',
+            'mailtype' => 'html',
+            'charset' => 'utf-8',
+            'newline' => "\r\n"
+        );    
+ 
+        //cargamos la configuración para enviar con gmail
+        $this->email->initialize($configGmail);
+ 
+        $this->email->from('consultas@dvigi.com.ar', 'DVIGI tu agua pura');
+        $this->email->to("$email_destino");
+        $this->email->subject("$asunto");
+        $this->email->message('<h2>Email enviado desde el Sistema DVIGI</h2><hr></br>'.  $cuerpo_mensaje);
+        $this->email->send();
+        //con esto podemos ver el resultado
+        //var_dump($this->email->print_debugger());
+        //print_r('entro');die();
     }
 
     public function admin_prod()
