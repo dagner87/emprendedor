bf382595843cc0a7e4ffb7a2da394214ccb3bec5
Dagner Alena Guerra <dalenag87@gmail.com>
2018-09-13 01:06:59 -0400

    inicioasdin

--- application/views/admin_general/page_inicioAdmin.php
+++ application/views/admin_general/page_inicioAdmin.php
@@ -18,8 +18,8 @@
                     <div class="row">
                     <div class="col-lg-3 col-sm-6 col-xs-12">
                         <div class="white-box">
-                            <h3 class="box-title">Nuevos Emprendedores</h3>
-                            <div class="text-right"> <span class="text-muted">Ventas</span>
+                            <h3 class="box-title">COMPRAS TOTALES</h3>
+                            <div class="text-right"> <span class="text-muted">Dinero</span>
                                 <h1><sup><i class="ti-arrow-up text-success"></i></sup> $12,000</h1> </div> <span class="text-success">20%</span>
                             <div class="progress m-b-0">
                                 <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:20%;"> <span class="sr-only">20% Complete</span> </div>
@@ -28,8 +28,8 @@
                     </div>
                     <div class="col-lg-3 col-sm-6 col-xs-12">
                         <div class="white-box">
-                            <h3 class="box-title">Weekly Sales</h3>
-                            <div class="text-right"> <span class="text-muted">Weekly Income</span>
+                            <h3 class="box-title">TOTAL DE EMPRENDEDORES</h3>
+                            <div class="text-right"> <span class="text-muted">Cantidad</span>
                                 <h1><sup><i class="ti-arrow-down text-danger"></i></sup> $5,000</h1> </div> <span class="text-danger">30%</span>
                             <div class="progress m-b-0">
                                 <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:30%;"> <span class="sr-only">230% Complete</span> </div>
@@ -38,8 +38,8 @@
                     </div>
                     <div class="col-lg-3 col-sm-6 col-xs-12">
                         <div class="white-box">
-                            <h3 class="box-title">Monthly Sales</h3>
-                            <div class="text-right"> <span class="text-muted">Monthly Income</span>
+                            <h3 class="box-title">NUEVOS EMPRENDEDORES</h3>
+                            <div class="text-right"> <span class="text-muted">Semana</span>
                                 <h1><sup><i class="ti-arrow-up text-info"></i></sup> $10,000</h1> </div> <span class="text-info">60%</span>
                             <div class="progress m-b-0">
                                 <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:60%;"> <span class="sr-only">20% Complete</span> </div>
@@ -48,8 +48,8 @@
                     </div>
                     <div class="col-lg-3 col-sm-6 col-xs-12">
                         <div class="white-box">
-                            <h3 class="box-title">Yearly Sales</h3>
-                            <div class="text-right"> <span class="text-muted">Yearly Income</span>
+                            <h3 class="box-title">NUEVOS EMPRENDEDORES</h3>
+                            <div class="text-right"> <span class="text-muted">Mes</span>
                                 <h1><sup><i class="ti-arrow-up text-inverse"></i></sup> $9,000</h1> </div> <span class="text-inverse">80%</span>
                             <div class="progress m-b-0">
                                 <div class="progress-bar progress-bar-inverse" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:80%;"> <span class="sr-only">230% Complete</span> </div>
