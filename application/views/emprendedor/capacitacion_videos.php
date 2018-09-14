7c299e4c67a0c1987bd633bbc49197805845dca4
Dagner Alena Guerra <dalenag87@gmail.com>
2018-09-14 14:02:44 -0400

    mejora del formulario evaluacion

--- application/views/emprendedor/capacitacion_videos.php
+++ application/views/emprendedor/capacitacion_videos.php
@@ -21,13 +21,16 @@
 
 
             if (!empty($list_cap)):
-                        $cont = 1;
+                        $cont = 0;
                         $videoact = $datos_emp->id_cap;
-                     foreach ($list_cap as $key): ?> 
+                     foreach ($list_cap as $key): 
+                        $cont ++;
+                        ?> 
+
                       <!--pestañas de videos-->    
                       <div class="panel panel-default">
                     <div class="panel-heading" role="tab" id="headingOne">
-                        <h4 class="panel-title"> <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne<?= $key->id_cap ?>" aria-expanded="true" aria-controls="collapseOne" class="font-bold">  Video # <?= $cont++ ?> </a> </h4> </div>
+                        <h4 class="panel-title"> <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne<?= $key->id_cap ?>" aria-expanded="true" aria-controls="collapseOne" class="font-bold">  Video # <?= $cont ?> </a> </h4> </div>
                     <div id="collapseOne<?= $key->id_cap ?>" class="panel-collapse collapse <?php if($key->id_cap == $videoact){ echo "in";}?>" role="tabpanel" aria-labelledby="headingOne">
                         <div class="panel-body"> 
                             <div class="col-md-12">
@@ -37,44 +40,23 @@
                                         <div class="col-sm-3">
                                             <a class="popup-youtube btn btn-default" title="Ver Video" href="<?= $key->url_video ?>"><img src="<?php echo base_url();?>assets/videos/<?= $key->imag_portada ?>" class="img-responsive" /></a></div>
                                         <div class="col-sm-9" id="slimtest1">
-                                            <p><?= $key->descripcion ?> </p>
+                                            
                                             <?php if($key->id_cap == $videoact):  ?> 
-                                                 <p><a class="popup-with-form btn btn-success" href="#test-form">Formulario de Evaluación #<?= $cont++ ?></a></p>
+                                                 <p><a class="popup-with-form btn btn-success" href="#test-form">Formulario de Evaluación #<?= $cont ?></a></p>
                                               <?php endif ?> 
                                             <div class="panel-group wiz-aco" id="accordion" role="tablist" aria-multiselectable="true">
                             <div class="panel panel-default">
                                 <div class="panel-heading" role="tab" id="headingOne">
                                     <h4 class="panel-title">
                       <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Collapsible Group Item #1
+                        Description del Video <?= $cont ?> 
                       </a>
                     </h4> </div>
                                 <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                                    <div class="panel-body"> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. </div>
+                                    <div class="panel-body"> <p><?= $key->descripcion ?> </p> </div>
                                 </div>
                             </div>
-                            <div class="panel panel-default">
-                                <div class="panel-heading" role="tab" id="headingTwo">
-                                    <h4 class="panel-title">
-                      <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Collapsible Group Item #2
-                      </a>
-                    </h4> </div>
-                                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-                                    <div class="panel-body"> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. </div>
-                                </div>
-                            </div>
-                            <div class="panel panel-default">
-                                <div class="panel-heading" role="tab" id="headingThree">
-                                    <h4 class="panel-title">
-                      <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Collapsible Group Item #3
-                      </a>
-                    </h4> </div>
-                                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-                                    <div class="panel-body"> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. </div>
-                                </div>
-                            </div>
+                           
                         </div>  
                                         </div>
 
